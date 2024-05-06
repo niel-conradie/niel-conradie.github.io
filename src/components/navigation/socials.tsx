ccddce831fcbfc1f53efd76e0af66273b9ccbf39
Niel Conradie <nielconradie93@gmail.com>
2024-05-06 15:06:37 +0200

    Tooltip

--- src/components/navigation/socials.tsx
+++ src/components/navigation/socials.tsx
@@ -3,6 +3,12 @@
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 import { socials } from "@/constants";
 
@@ -13,18 +19,27 @@ export default function Socials() {
         {socials.map((link, index) => {
           return (
             <li key={index}>
-              <Button
-                className="transition-all duration-300 hover:scale-110 hover:text-primary"
-                variant="ghost"
-                size="icon"
-                aria-label={link.name}
-                asChild
-              >
-                <Link href={link.href}>
-                  <span className="text-xl">{link.icon}</span>
-                  <span className="sr-only">={link.name}</span>
-                </Link>
-              </Button>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      className="transition-all duration-300 hover:scale-110 hover:text-primary"
+                      variant="ghost"
+                      size="icon"
+                      aria-label={link.name}
+                      asChild
+                    >
+                      <Link href={link.href}>
+                        <span className="text-xl">{link.icon}</span>
+                        <span className="sr-only">={link.name}</span>
+                      </Link>
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{link.name}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </li>
           );
         })}
--- src/components/theme/theme-toggle.tsx
+++ src/components/theme/theme-toggle.tsx
@@ -4,6 +4,12 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -11,16 +17,25 @@ export default function ThemeToggle() {
   const onClickToggle = () => setTheme(theme === "dark" ? "light" : "dark");
 
   return (
-    <Button
-      className="transition-all duration-300 hover:scale-110 hover:text-primary"
-      variant="ghost"
-      size="icon"
-      aria-label="Toggle Theme"
-      onClick={onClickToggle}
-    >
-      <Sun className="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle Theme</span>
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            className="transition-all duration-300 hover:scale-110 hover:text-primary"
+            variant="ghost"
+            size="icon"
+            aria-label="Toggle Theme"
+            onClick={onClickToggle}
+          >
+            <Sun className="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle Theme</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Toggle Theme</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
