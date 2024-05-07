9e0bcb5d13c14db2cb3655cf27711140365dc248
Niel Conradie <nielconradie93@gmail.com>
2024-05-07 18:09:29 +0200

    Carousel

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,29 +1,12 @@
 "use client";
 
-import { useState } from "react";
-
 import { motion } from "framer-motion";
 
 import { Projects } from "@/components/projects";
-import { Button } from "@/components/ui/button";
 
 import { fadeIn } from "@/utils";
 
-import { filters, projects } from "@/constants";
-
 export default function ProjectsPage() {
-  const [search, setSearch] = useState<string>("");
-
-  function filterProjects(search: string) {
-    return projects.filter((project) =>
-      project.tags.some((tag) =>
-        tag.toLowerCase().includes(search.toLowerCase()),
-      ),
-    );
-  }
-
-  const filteredProjects = filterProjects(search);
-
   return (
     <main className="flex min-h-[calc(100svh)] items-center justify-center px-4 py-[50px] sm:pb-4 sm:pt-[50px]">
       <motion.div
@@ -36,33 +19,8 @@ export default function ProjectsPage() {
         initial="hidden"
         animate="show"
       >
-        <h2 className="mb-4 text-center text-2xl sm:text-4xl">Projects</h2>
-        <div className="mb-4 text-center">
-          {filters.map((filter, index) => {
-            return (
-              <Button
-                key={index}
-                className="transition-all duration-300"
-                variant="ghost"
-                aria-label={filter.name}
-                onClick={() => setSearch(filter.value)}
-              >
-                <span
-                  className={
-                    search === filter.value
-                      ? "text-primary hover:text-primary"
-                      : ""
-                  }
-                >
-                  {filter.name}
-                </span>
-              </Button>
-            );
-          })}
-        </div>
-
         <div className="">
-          <Projects projects={filteredProjects} />
+          <Projects />
         </div>
       </motion.div>
     </main>
--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+
+import Autoplay from "embla-carousel-autoplay";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -16,75 +19,142 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
+  type CarouselApi,
 } from "@/components/ui/carousel";
 
-interface Project {
-  name: string;
-  description: string;
-  image: string;
-  pages: string;
-  github: string;
-  tags: string[];
-}
+import { filters, projects } from "@/constants";
 
-interface Projects {
-  projects: Project[];
-}
+export default function Projects() {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+  const [search, setSearch] = useState("");
 
-export default function Projects({ projects }: Projects) {
+  function filterProjects(search: string) {
+    return projects.filter((project) =>
+      project.tags.some((tag) =>
+        tag.toLowerCase().includes(search.toLowerCase()),
+      ),
+    );
+  }
+
+  const filteredProjects = filterProjects(search);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("slidesInView", () => {
+      setCount(api.scrollSnapList().length);
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  const plugin = useRef(Autoplay({ delay: 10000, stopOnInteraction: true }));
+
   return (
-    <Carousel className="w-[350px] sm:w-[550px] xl:w-[1400px]">
-      <CarouselContent>
-        {projects.map((project, index) => (
-          <CarouselItem key={index} className="xl:basis-1/3">
-            <Card className="h-[355px] cursor-grab sm:h-[470px] xl:h-[415px]">
-              <CardHeader className="text-center">
-                <CardTitle>{project.name}</CardTitle>
-                <CardDescription>{project.description}</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <Image
-                  src={project.image}
-                  alt={project.name}
-                  width={500}
-                  height={500}
-                />
-              </CardContent>
-              <CardFooter>
-                <div className="h-full w-full">
-                  <div className="flex items-center justify-center gap-x-6">
-                    <Button
-                      className="transition-all duration-300"
-                      variant="outline"
-                      aria-label="GitHub"
-                      asChild
-                    >
-                      <Link href={project.github}>
-                        <span className="text-primary">GitHub</span>
-                      </Link>
-                    </Button>
-                    <Button
-                      className="transition-all duration-300"
-                      variant="outline"
-                      aria-label="Pages"
-                      asChild
-                    >
-                      <Link href={project.pages}>
-                        <span className="text-primary">Pages</span>
-                      </Link>
-                    </Button>
-                  </div>
-                </div>
-              </CardFooter>
-            </Card>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-
-      <div className="hidden sm:block">
-        <CarouselPrevious />
-        <CarouselNext />
+    <>
+      <h2 className="mb-4 text-center text-2xl tracking-wider sm:text-4xl">
+        Projects
+      </h2>
+      <div className="mb-4 text-center">
+        {filters.map((filter, index) => {
+          return (
+            <Button
+              key={index}
+              className="transition-all duration-300"
+              variant="ghost"
+              aria-label={filter.name}
+              onClick={() => setSearch(filter.value)}
+            >
+              <span
+                className={`tracking-wider ${search === filter.value ? "text-primary hover:text-primary" : ""}`}
+              >
+                {filter.name}
+              </span>
+            </Button>
+          );
+        })}
       </div>
-    </Carousel>
+
+      <Carousel
+        className="w-[350px] sm:w-[550px] xl:w-[1400px]"
+        setApi={setApi}
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+        plugins={[plugin.current]}
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+      >
+        <CarouselContent>
+          {filteredProjects.map((project, index) => (
+            <CarouselItem key={index} className="xl:basis-1/3">
+              <Card className="h-[355px] cursor-grab sm:h-[470px] xl:h-[415px]">
+                <CardHeader className="text-center">
+                  <CardTitle className="tracking-wider">
+                    {project.name}
+                  </CardTitle>
+                  <CardDescription className="tracking-wider">
+                    {project.description}
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Image
+                    src={project.image}
+                    alt={project.name}
+                    width={500}
+                    height={500}
+                  />
+                </CardContent>
+                <CardFooter>
+                  <div className="h-full w-full">
+                    <div className="flex items-center justify-center gap-x-6">
+                      <Button
+                        className="transition-all duration-300"
+                        variant="outline"
+                        aria-label="GitHub"
+                        asChild
+                      >
+                        <Link href={project.github}>
+                          <span className="tracking-wider text-primary">
+                            GitHub
+                          </span>
+                        </Link>
+                      </Button>
+                      <Button
+                        className="transition-all duration-300"
+                        variant="outline"
+                        aria-label="Pages"
+                        asChild
+                      >
+                        <Link href={project.pages}>
+                          <span className="tracking-wider text-primary">
+                            Pages
+                          </span>
+                        </Link>
+                      </Button>
+                    </div>
+                  </div>
+                </CardFooter>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <div className="hidden sm:block">
+          <CarouselPrevious />
+          <CarouselNext />
+        </div>
+      </Carousel>
+
+      <p className="mt-6 text-center text-sm text-muted-foreground sm:hidden">
+        {current} of {count}
+      </p>
+    </>
   );
 }
