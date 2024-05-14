f1f9f32add76ca7ae13c72cdd6a5caa82d8bc7cc
Niel Conradie <nielconradie93@gmail.com>
2024-05-14 13:12:08 +0200

    Cube
    Test

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,19 @@
-import { LoaderProvider, Logo } from "@/components/utils";
+import { Cube, LoaderProvider, Logo } from "@/components/utils";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-[calc(100dvh)] items-center justify-center px-4 py-[3.125rem] sm:pb-4 sm:pt-[3.125rem]">
       <LoaderProvider>
-        <Logo />
+        <div className="">
+          <div className="">
+            <h2 className=""></h2>
+            <p className=""></p>
+          </div>
+
+          <div className="">
+            <Cube />
+          </div>
+        </div>
       </LoaderProvider>
     </main>
   );
--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -1,3 +1,4 @@
+export { default as Cube } from "@/components/utils/cube";
 export { default as Loader } from "@/components/utils/loader";
 export { default as LoaderProvider } from "@/components/utils/loader-provider";
 export { default as Logo } from "@/components/utils/logo";
